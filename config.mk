81906f1073b73ced7c206290abb3594f5d057722
hurtcopain <jacksummers715@gmail.com>
2024-10-30 21:09:18 -0400

    Add back mainline

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/google/pixel-additional/products/telephony.mk)
 
 # Inherit from apex config
-#$(call inherit-product, vendor/google/mainline_modules/config.mk)
+$(call inherit-product, vendor/google/mainline_modules/config.mk)
 
 # Inherit from gms config
 $(call inherit-product, vendor/google/pixel-additional/products/gms.mk)
